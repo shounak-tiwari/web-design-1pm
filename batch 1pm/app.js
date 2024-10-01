5cd30dc1784618c9c73fe55ea0ca6738baa246f1
Akash Tiwari <154492266+shounak-tiwari@users.noreply.github.com>
2024-10-01 13:58:58 +0530

    Add files via upload

---
+++ batch 1pm/app.js
@@ -0,0 +1,10 @@
+
+let a = document.getElementById('p1').textContent;
+let b = document.getElementById('p2').textContent;
+a =parseInt(a)
+b = parseInt(b)
+
+document.getElementById("name").value ="Akash"
+
+
+// variables :  
